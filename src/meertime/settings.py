56b3bd1e2c042f65d347d63715c7a60d5c14871a
Stefan Oslowski <stefanoslowski@swin.edu.au>
2020-07-30 15:32:07 +1000

    Optionally, read .env file, explicit cast of DEBUG to bool
    Summary: as above
    
    Test Plan: make sure pythonn manage.py runserver
    
    Reviewers: mr-asher
    
    Reviewed By: mr-asher
    
    Differential Revision: https://phab.gw-cloud.org/D44

--- src/meertime/settings.py
+++ src/meertime/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import environ
-env = environ.Env()
-DEBUG = env("DEBUG", default=False)
+
+env = environ.Env(DEBUG=(bool, False),)
+environ.Env.read_env(".env")
+
+DEBUG = env("DEBUG")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7-0w1bqo76#=hebhn@!1@t+#3p!p3#ti_138+dn=9y1&6ipno3'
+SECRET_KEY = "7-0w1bqo76#=hebhn@!1@t+#3p!p3#ti_138+dn=9y1&6ipno3"
 
 ALLOWED_HOSTS = []
 
@@ -31,58 +34,58 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dataportal.apps.DataportalConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "dataportal.apps.DataportalConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'meertime.urls'
+ROOT_URLCONF = "meertime.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'meertime.wsgi.application'
+WSGI_APPLICATION = "meertime.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': env("DB_NAME", default="meertime"),
-        'USER': env("DB_USER", default="bWVlcnRpbWU="),
-        'PASSWORD': env("DB_PASS"),
-        'HOST': env("DB_HOST", default="mysql"),
-        'PORT': env("DB_PORT", default="3306"),
-     }
+    "default": {
+        "ENGINE": "mysql.connector.django",
+        "NAME": env("DB_NAME", default="meertime"),
+        "USER": env("DB_USER", default="bWVlcnRpbWU="),
+        "PASSWORD": env("DB_PASS"),
+        "HOST": env("DB_HOST", default="mysql"),
+        "PORT": env("DB_PORT", default="3306"),
+    }
 }
 
 
@@ -91,29 +94,23 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 7,
-        }
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {"min_length": 7,},
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -125,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/static/'
-STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
+STATIC_URL = "/static/"
 
-LOGIN_REDIRECT_URL = '/MeerTime'
+LOGIN_REDIRECT_URL = "/MeerTime"
