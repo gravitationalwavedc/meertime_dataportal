dc5e9db648a92d028d0e9b92980afab578bc8afc
Stefan Oslowski <stefanoslowski@swin.edu.au>
2020-08-02 15:39:22 +1000

    Add ingress, use vault secret key from vault
    Summary: Also, tweak .gitignore
    
    Test Plan: argocd
    
    Reviewers: jesmigel
    
    Reviewed By: jesmigel
    
    Differential Revision: https://phab.gw-cloud.org/D52

--- src/meertime/settings.py
+++ src/meertime/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "7-0w1bqo76#=hebhn@!1@t+#3p!p3#ti_138+dn=9y1&6ipno3"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
