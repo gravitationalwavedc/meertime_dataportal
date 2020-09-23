1ce7fbf04c4f2236ce5c8dddafb6ef6c60c2e32a
Stefan Oslowski <stefanoslowski@swin.edu.au>
2020-09-23 10:49:52 +1000

    Resolve issues with poetry and variable names
    Summary:
    Rollback to DB_HOST instead of MYSQL_HOST. The latter can break the mysql image and thus is incompatible with setting vars via vault secrets without duplicating secrets (one copy for django and one for mysql).
    Remove double entry of django-cors-headers in the poetry.lock
    Remove double entry of django-debug-toolbar in pyproject.toml, including erroneous inclusion in the dependencies
    Update requirements.txt
    
    Test Plan: pytest, poetry update
    
    Reviewers: mr-asher
    
    Reviewed By: mr-asher
    
    Differential Revision: https://phab.gw-cloud.org/D177

--- src/meertime/settings/database.py
+++ src/meertime/settings/database.py
@@ -9,7 +9,7 @@ DATABASES = {
         "NAME": env("MYSQL_DATABASE"),
         "USER": env("MYSQL_USER"),
         "PASSWORD": env("MYSQL_PASSWORD"),
-        "HOST": env("MYSQL_HOST"),
+        "HOST": env("DB_HOST"),
         "PORT": env("MYSQL_PORT", default=3306),
     }
 }
