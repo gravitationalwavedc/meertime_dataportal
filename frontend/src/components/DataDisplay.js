a8fb5164861c51c9ef72529798d6b9ec3c270039
Asher <jasherleslie@gmail.com>
2022-07-05 13:02:36 +1000

    Update titles on Single Observation Page to match google doc request.

--- frontend/src/components/DataDisplay.js
+++ frontend/src/components/DataDisplay.js
@@ -1,11 +1,11 @@
 import Col from 'react-bootstrap/Col';
 import React from 'react';
 
-const DataDisplay = ({ title, value, full }) => 
+const DataDisplay = ({ title, value, full }) =>
     <Col lg={full ? 12 : 2} md={full ? 12 : 3} sm xs>
-        <p className="mb-1 text-primary-600">{ title }</p>
-        <h4>{ value ? value : 'N/A' }</h4>
-    </Col>;
+        <p className="mb-1 text-primary-600">{title}</p>
+        <h4>{value ? value : 'N/A'}</h4>
+    </Col >;
 
 DataDisplay.defaultProps = {
     full: false
