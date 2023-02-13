9663abf2a401b8d817419cab4383c962a2a28943
Asher <jasherleslie@gmail.com>
2023-02-13 11:16:17 +1100

    Remove unneeded useEffect.

--- frontend/src/components/ImageGrid.js
+++ frontend/src/components/ImageGrid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ComparisonImageGrid from './ComparisonImageGrid';
 import LightBox from 'react-image-lightbox';
 import PlotImage from './PlotImage';
@@ -6,28 +6,21 @@ import ToaImages from './ToaImages';
 
 const ImageGrid = ({ images, project }) => {
     const [isLightBoxOpen, setIsLightBoxOpen] = useState(false);
-    const [lightBoxImages, setLightBoxImages] = useState({ images: [], imagesIndex: 0 });
-    const [rawImages, setRawImages] = useState([]);
-    const [processedImages, setProcessedImages] = useState([]);
 
-    useEffect(() => {
-        const newRawImages = images.edges.filter(
-            ({ node }) => node.process.toLowerCase() === 'raw' && node.resolution === 'hi'
-        );
+    const rawImages = images.edges.filter(
+        ({ node }) => node.process.toLowerCase() === 'raw' && node.resolution === 'hi'
+    );
 
-        const newProcessedImages = images.edges.filter(
-            ({ node }) => node.process.toLowerCase() !== 'raw' && node.process.toLowerCase() === project.toLowerCase()
-        );
+    const processedImages = images.edges.filter(
+        ({ node }) => node.process.toLowerCase() !== 'raw' && node.process.toLowerCase() === project.toLowerCase()
+    );
 
-        const newLightBoxImages = [
-            ...newRawImages.map(({ node }) => node.url),
-            ...newProcessedImages.map(({ node }) => node.url)
-        ];
+    const newlightBoxImages = [
+        ...rawImages.map(({ node }) => node.url),
+        ...processedImages.map(({ node }) => node.url)
+    ];
 
-        setRawImages(newRawImages);
-        setProcessedImages(newProcessedImages);
-        setLightBoxImages({ images: newLightBoxImages, imagesIndex: 0 });
-    }, [project, images.edges]);
+    const [lightBoxImages, setLightBoxImages] = useState({ images: newlightBoxImages, imagesIndex: 0 });
 
     const openLightBox = (imageUrl) => {
         const images = lightBoxImages.images;
@@ -41,43 +34,43 @@ const ImageGrid = ({ images, project }) => {
             processedImages={processedImages}
             handleLightBox={openLightBox}
         />
-        { processedImages.length > 0 ? <ComparisonImageGrid 
+        {processedImages.length > 0 ? <ComparisonImageGrid
             rawImages={rawImages}
             processedImages={processedImages}
             openLightBox={openLightBox}
-            project={project}/> : 
+            project={project} /> :
             rawImages.map(({ node }) =>
-                <PlotImage 
+                <PlotImage
                     key={node.url}
                     imageData={node}
-                    handleClick={() => openLightBox(node.url)} 
+                    handleClick={() => openLightBox(node.url)}
                 />
             )
-        } 
+        }
         {isLightBoxOpen &&
-                <LightBox
-                    mainSrc={`${process.env.REACT_APP_MEDIA_URL}${lightBoxImages.images[lightBoxImages.imagesIndex]}`}
-                    nextSrc={lightBoxImages.images[(lightBoxImages.imagesIndex + 1) % lightBoxImages.images.length]}
-                    prevSrc={
-                        lightBoxImages.images[(lightBoxImages.imagesIndex + lightBoxImages.images.length - 1)
-                        % lightBoxImages.images.length
-                        ]}
-                    onCloseRequest={() => setIsLightBoxOpen(false)}
-                    onMovePrevRequest={() =>
-                        setLightBoxImages({
-                            images: lightBoxImages.images,
-                            imagesIndex: (
-                                lightBoxImages.imagesIndex + lightBoxImages.images.length - 1
-                            ) % lightBoxImages.images.length,
-                        })
-                    }
-                    onMoveNextRequest={() =>
-                        setLightBoxImages({
-                            images: lightBoxImages.images,
-                            imagesIndex: (lightBoxImages.imagesIndex + 1) % lightBoxImages.images.length,
-                        })
-                    }
-                />
+            <LightBox
+                mainSrc={`${process.env.REACT_APP_MEDIA_URL}${lightBoxImages.images[lightBoxImages.imagesIndex]}`}
+                nextSrc={lightBoxImages.images[(lightBoxImages.imagesIndex + 1) % lightBoxImages.images.length]}
+                prevSrc={
+                    lightBoxImages.images[(lightBoxImages.imagesIndex + lightBoxImages.images.length - 1)
+                    % lightBoxImages.images.length
+                    ]}
+                onCloseRequest={() => setIsLightBoxOpen(false)}
+                onMovePrevRequest={() =>
+                    setLightBoxImages({
+                        images: lightBoxImages.images,
+                        imagesIndex: (
+                            lightBoxImages.imagesIndex + lightBoxImages.images.length - 1
+                        ) % lightBoxImages.images.length,
+                    })
+                }
+                onMoveNextRequest={() =>
+                    setLightBoxImages({
+                        images: lightBoxImages.images,
+                        imagesIndex: (lightBoxImages.imagesIndex + 1) % lightBoxImages.images.length,
+                    })
+                }
+            />
         }
     </React.Fragment>;
 };
