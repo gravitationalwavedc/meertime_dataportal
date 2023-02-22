a25c1e18f8ea277c4cc0ce49d354af293256ecd1
Asher <jasherleslie@gmail.com>
2023-02-22 14:25:13 +1100

    Fix npm tests.

--- frontend/src/components/FoldDetailTable.test.js
+++ frontend/src/components/FoldDetailTable.test.js
@@ -25,6 +25,22 @@ jest.mock('found', () => ({
     } }) 
 }));
 
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
+
 describe('the fold table component', () => {
     const data = {
         foldObservationDetails: {
