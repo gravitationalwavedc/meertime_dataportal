bb75ef8b300adf1ed267f26f3897d1ed6e5d976e
Asher <jasherleslie@gmail.com>
2023-03-01 11:21:08 +1100

    Add flux value but still has an issue with how molonglo stores values.

--- frontend/src/components/FoldDetailTable.test.js
+++ frontend/src/components/FoldDetailTable.test.js
@@ -25,23 +25,9 @@ jest.mock('found', () => ({
     } }) 
 }));
 
-const { ResizeObserver } = window;
-
-beforeEach(() => {
-  delete window.ResizeObserver;
-  window.ResizeObserver = jest.fn().mockImplementation(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-  }));
-});
-
-afterEach(() => {
-  window.ResizeObserver = ResizeObserver;
-  jest.restoreAllMocks();
-});
 
 describe('the fold table component', () => {
+
     const data = {
         foldObservationDetails: {
             totalObservations: 4,
