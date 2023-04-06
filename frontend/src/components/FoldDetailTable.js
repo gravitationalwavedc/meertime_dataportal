959462d2bd41b265068c3770fd81bd756ad4bd96
Asher <jasherleslie@gmail.com>
2023-04-06 15:54:08 +1000

    Correct formatting for file using project eslint.

--- frontend/src/components/FoldDetailTable.js
+++ frontend/src/components/FoldDetailTable.js
@@ -1,6 +1,11 @@
 import { Button, ButtonGroup, Col, Row } from 'react-bootstrap';
 import React, { useState } from 'react';
-import { columnsSizeFilter, formatDDMonYYYY, formatUTC, meerWatchLink } from '../helpers';
+import {
+    columnsSizeFilter,
+    formatDDMonYYYY,
+    formatUTC,
+    meerWatchLink,
+} from '../helpers';
 import { meertime, molonglo } from '../telescopes';
 import DataView from './DataView';
 import Ephemeris from './Ephemeris';
@@ -10,95 +15,130 @@ import ReactMarkdown from 'react-markdown';
 import { useScreenSize } from '../context/screenSize-context';
 
 /* eslint-disable complexity */
-const FoldDetailTable = ({ data: { foldObservationDetails }, jname, mainProject }) => {
+const FoldDetailTable = (
+    { data: { foldObservationDetails }, jname, mainProject },
+) => {
     const { screenSize } = useScreenSize();
     const allRows = foldObservationDetails.edges.reduce(
         (result, edge) => [
-            ...result, 
-            { 
-                ...edge.node, 
+            ...result,
+            {
+                ...edge.node,
                 key: `${edge.node.utc}:${edge.node.beam}`,
                 jname: jname,
-                embargo: edge.node.restricted ? 'Embargoed until ' + formatDDMonYYYY(edge.node.embargoEndDate) : '',
+                embargo: edge.node.restricted
+                    ? 'Embargoed until ' + formatDDMonYYYY(edge.node.embargoEndDate)
+                    : '',
                 utc: formatUTC(edge.node.utc),
-                plotLink: `${process.env.REACT_APP_BASE_URL}/${jname}/${formatUTC(edge.node.utc)}/${edge.node.beam}/`,
-                action: !edge.node.restricted ? <ButtonGroup vertical>
-                    <Link 
-                        to={`${process.env.REACT_APP_BASE_URL}/${jname}/${formatUTC(edge.node.utc)}/${edge.node.beam}/`}
-                        size="sm" 
-                        variant="outline-secondary" as={Button}>View</Link> 
-                    <Link 
-                        to={`${process.env.REACT_APP_BASE_URL}/session/${formatUTC(edge.node.utc)}/`}
-                        size="sm" 
-                        variant="outline-secondary" as={Button}>View session</Link> 
-                </ButtonGroup> :
-                    <Button
+                plotLink: `${process.env.REACT_APP_BASE_URL}/${jname}/${
+                    formatUTC(edge.node.utc)
+                }/${edge.node.beam}/`,
+                action: !edge.node.restricted
+                    ? <ButtonGroup vertical>
+                        <Link
+                            to={`${process.env.REACT_APP_BASE_URL}/${jname}/${
+                                formatUTC(edge.node.utc)
+                            }/${edge.node.beam}/`}
+                            size="sm"
+                            variant="outline-secondary"
+                            as={Button}
+                        >
+              View
+                        </Link>
+                        <Link
+                            to={`${process.env.REACT_APP_BASE_URL}/session/${
+                                formatUTC(edge.node.utc)
+                            }/`}
+                            size="sm"
+                            variant="outline-secondary"
+                            as={Button}
+                        >
+              View session
+                        </Link>
+                    </ButtonGroup>
+                    : <Button
                         size="sm"
-                        variant="outline-dark">
-                        <span className='small'>Embargoed<br/>until<br/>{formatDDMonYYYY(edge.node.embargoEndDate) }
+                        variant="outline-dark"
+                    >
+                        <span className="small">
+              Embargoed<br />until<br />
+                            {formatDDMonYYYY(edge.node.embargoEndDate)}
                         </span>
-                    </Button>
-
-            }
-        ], []
+                    </Button>,
+            },
+        ],
+        [],
     );
 
     const [rows, setRows] = useState(allRows);
     const [ephemerisVisible, setEphemerisVisible] = useState(false);
 
-    const ephemeris = foldObservationDetails.edges[foldObservationDetails.edges.length -1 ].node.ephemeris;
-    const ephemerisUpdated = 
-        foldObservationDetails.edges[foldObservationDetails.edges.length -1 ].node.ephemerisIsUpdatedAt;
+    const ephemeris =
+    foldObservationDetails.edges[foldObservationDetails.edges.length - 1].node
+        .ephemeris;
+    const ephemerisUpdated =
+    foldObservationDetails.edges[foldObservationDetails.edges.length - 1].node
+        .ephemerisIsUpdatedAt;
 
-    const columns = mainProject === 'MONSPSR' ? molonglo.columns : meertime.columns;
+    const columns = mainProject === 'MONSPSR'
+        ? molonglo.columns
+        : meertime.columns;
 
     const columnsSizeFiltered = columnsSizeFilter(columns, screenSize);
 
     // totalEstimatedDiskSpace is a human readable formatted byte string in the form of "900.2\u00a0MB".
     // We split on this character so we can use the number and the units separately.
-    const [size, sizeFormat] = foldObservationDetails.totalEstimatedDiskSpace.split('\u00a0');
+    const [size, sizeFormat] = foldObservationDetails.totalEstimatedDiskSpace
+        .split('\u00a0');
 
     const handleBandFilter = (band) => {
-        if(band.toLowerCase() === 'all') {
+        if (band.toLowerCase() === 'all') {
             setRows(allRows);
             return;
         }
 
-        const newRows = allRows.filter((row) => row.band.toLowerCase() === band.toLowerCase());
+        const newRows = allRows.filter((row) =>
+            row.band.toLowerCase() === band.toLowerCase()
+        );
         setRows(newRows);
     };
 
     const handleProjectFilter = (project) => {
-        if(project === 'All') {
+        if (project === 'All') {
             setRows(allRows);
             return;
         }
 
-        const newRows = allRows.filter((row) => row.project.toLowerCase() === project.toLowerCase());
+        const newRows = allRows.filter((row) =>
+            row.project.toLowerCase() === project.toLowerCase()
+        );
         setRows(newRows);
-
     };
 
     const summaryData = [
-        { title: 'Observations', value: foldObservationDetails.totalObservations }, 
-        { title: 'Projects', value: foldObservationDetails.totalProjects }, 
-        { title: 'Timespan [days]', value: foldObservationDetails.totalTimespanDays }, 
-        { title: 'Hours', value: foldObservationDetails.totalObservationHours }, 
-        { title: `Size [${sizeFormat}]`, value: size }, 
+        { title: 'Observations', value: foldObservationDetails.totalObservations },
+        { title: 'Projects', value: foldObservationDetails.totalProjects },
+        {
+            title: 'Timespan [days]',
+            value: foldObservationDetails.totalTimespanDays,
+        },
+        { title: 'Hours', value: foldObservationDetails.totalObservationHours },
+        { title: `Size [${sizeFormat}]`, value: size },
     ];
 
     const downloadEphemeris = async () => {
         const link = document.createElement('a');
-        link.href = `${process.env.REACT_APP_MEDIA_URL}${foldObservationDetails.ephemerisLink}`;
+        link.href =
+      `${process.env.REACT_APP_MEDIA_URL}${foldObservationDetails.ephemerisLink}`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     };
 
-
     const downloadToas = async () => {
         const link = document.createElement('a');
-        link.href = `${process.env.REACT_APP_MEDIA_URL}${foldObservationDetails.toasLink}`;
+        link.href =
+      `${process.env.REACT_APP_MEDIA_URL}${foldObservationDetails.toasLink}`;
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -113,51 +153,54 @@ const FoldDetailTable = ({ data: { foldObservationDetails }, jname, mainProject 
             </Row>
             <Row>
                 <Col>
-                    <Button 
+                    <Button
                         size="sm"
-                        variant="outline-secondary" 
+                        variant="outline-secondary"
                         className="mr-2 mb-2"
                         disabled={ephemeris ? false : true}
-                        onClick={() => setEphemerisVisible(true)}>
-                        { ephemeris ? 
-                            'View folding ephemeris' : 'Folding ephemeris unavailable'}
+                        onClick={() => setEphemerisVisible(true)}
+                    >
+                        {ephemeris
+                            ? 'View folding ephemeris'
+                            : 'Folding ephemeris unavailable'}
                     </Button>
-                    { mainProject !== 'MONSPSR' && <Button
+                    {mainProject !== 'MONSPSR' && <Button
                         size="sm"
                         className="mr-2 mb-2"
                         as="a"
                         href={meerWatchLink(jname)}
-                        variant="outline-secondary">
-                        View MeerWatch
-                    </Button> }
-                    { localStorage.isStaff === 'true' && foldObservationDetails.ephemerisLink &&
-                        <Button
-                            size="sm"
-                            className="mr-2 mb-2"
-                            variant="outline-secondary"
-                            onClick={() => downloadEphemeris()}
-                        >
-                            Download ephemeris
-                        </Button>
-                    }
-                    { localStorage.isStaff === 'true' && foldObservationDetails.toasLink &&
-                        <Button
-                            size="sm"
-                            className="mr-2 mb-2"
-                            variant="outline-secondary"
-                            onClick={() => downloadToas()}
-                        >
-                            Download TOAs
-                        </Button>
-                    }
+                        variant="outline-secondary"
+                    >
+            View MeerWatch
+                    </Button>}
+                    {localStorage.isStaff === 'true' &&
+            foldObservationDetails.ephemerisLink &&
+            <Button
+                size="sm"
+                className="mr-2 mb-2"
+                variant="outline-secondary"
+                onClick={() => downloadEphemeris()}
+            >
+              Download ephemeris
+            </Button>}
+                    {localStorage.isStaff === 'true' && foldObservationDetails.toasLink &&
+            <Button
+                size="sm"
+                className="mr-2 mb-2"
+                variant="outline-secondary"
+                onClick={() => downloadToas()}
+            >
+              Download TOAs
+            </Button>}
                 </Col>
             </Row>
-            { ephemeris && <Ephemeris 
-                ephemeris={ephemeris} 
+            {ephemeris && <Ephemeris
+                ephemeris={ephemeris}
                 updated={ephemerisUpdated}
                 show={ephemerisVisible}
-                setShow={setEphemerisVisible} />}
-            <DataView 
+                setShow={setEphemerisVisible}
+            />}
+            <DataView
                 summaryData={summaryData}
                 columns={columnsSizeFiltered}
                 rows={rows}
