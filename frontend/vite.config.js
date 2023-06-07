201925043643362ed8dad7ae2e9f0302a3188f1c
Asher <jasherleslie@gmail.com>
2023-06-07 21:09:38 +1000

    Fix coverage in ci.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,15 @@ export default defineConfig({
   plugins: [relay, react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.jsx',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.jsx",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    coverage: {
+      provider: "istanbul",
+      reporter: ["html", "cobertura"],
+      reportsDirectory: "./reports",
+    },
   },
 });
