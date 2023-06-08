100779bda037a1a30766120c176c3e2052cb3e51
NickSwainston <nickaswainston@gmail.com>
2023-06-08 09:53:40 +0800

    Registered some models on the admin page for easier manual data tests

--- backend/dataportal/admin.py
+++ backend/dataportal/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Pulsar
+from .models import Pulsar, MainProject, Project
 
 admin.site.register(Pulsar)
+admin.site.register(MainProject)
+admin.site.register(Project)
 
