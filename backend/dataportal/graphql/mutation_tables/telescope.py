f95950cf4dce349686946f15bffbcc0bab669e2d
NickSwainston <nickaswainston@gmail.com>
2023-06-08 09:59:27 +0800

    Updated telescope mutations

--- backend/dataportal/graphql/mutation_tables/telescope.py
+++ backend/dataportal/graphql/mutation_tables/telescope.py
@@ -1,35 +1,45 @@
 import graphene
+from graphene_django import DjangoObjectType
 from graphql_jwt.decorators import permission_required
 
-from dataportal.models import Telescopes
-from .types import TelescopesInput, TelescopesType
+from dataportal.models import Telescope
+from datetime import timedelta
+
+
+class TelescopeType(DjangoObjectType):
+    class Meta:
+        model = Telescope
+
+
+class TelescopeInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
 
 
 class CreateTelescope(graphene.Mutation):
     class Arguments:
-        input = TelescopesInput(required=True)
+        input = TelescopeInput(required=True)
 
-    telescope = graphene.Field(TelescopesType)
+    telescope = graphene.Field(TelescopeType)
 
     @classmethod
-    @permission_required("dataportal.add_telescopes")
+    @permission_required("dataportal.add_telescope")
     def mutate(cls, self, info, input):
-        telescope, _ = Telescopes.objects.get_or_create(**input.__dict__)
+        telescope, _ = Telescope.objects.get_or_create(**input.__dict__)
         return CreateTelescope(telescope=telescope)
 
 
 class UpdateTelescope(graphene.Mutation):
     class Arguments:
         id = graphene.Int(required=True)
-        input = TelescopesInput(required=True)
+        input = TelescopeInput(required=True)
 
-    telescope = graphene.Field(TelescopesType)
+    telescope = graphene.Field(TelescopeType)
 
     @classmethod
-    @permission_required("dataportal.add_telescopes")
+    @permission_required("dataportal.add_telescope")
     def mutate(cls, self, info, id, input):
         try:
-            telescope = Telescopes.objects.get(pk=id)
+            telescope = Telescope.objects.get(pk=id)
             for key, val in input.__dict__.items():
                 setattr(telescope, key, val)
             telescope.save()
@@ -45,9 +55,9 @@ class DeleteTelescope(graphene.Mutation):
     ok = graphene.Boolean()
 
     @classmethod
-    @permission_required("dataportal.add_telescopes")
+    @permission_required("dataportal.add_telescope")
     def mutate(cls, self, info, id):
-        Telescopes.objects.get(pk=id).delete()
+        Telescope.objects.get(pk=id).delete()
         return cls(ok=True)
 
 
