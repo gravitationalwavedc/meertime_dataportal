14453bb5edaf5bb714a13ee55c2c8a07d57aca5a
Asher <jasherleslie@gmail.com>
2023-06-15 14:07:34 +1000

    minify with terser instead of esbuild.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [relay, react()],
-  define: {
-    global: "window",
+  build: {
+    minify: "terser",
   },
   test: {
     globals: true,
