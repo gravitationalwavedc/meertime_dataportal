8667c7f3caeb48d85791c3d8f6cb0006d5bead3f
NickSwainston <nickaswainston@gmail.com>
2023-06-15 10:19:17 +0800

    Cleaned up unusued files

--- backend/dataportal/graphql/rfis/types.py
+++
@@ -1,8 +0,0 @@
-from graphene_django import DjangoObjectType
-
-from dataportal.models import Rfis
-
-
-class RfisType(DjangoObjectType):
-    class Meta:
-        model = Rfis
--- backend/ingest-ajameson/database/util.py
+++
@@ -1,8 +0,0 @@
-def singular_dict(output):
-    """convert dict[key][0] to dict[key]"""
-    if output is None:
-        return None
-    output_dict = []
-    for k in output.keys():
-        output_dict[k] = output[k][0]
-    return output_dict
