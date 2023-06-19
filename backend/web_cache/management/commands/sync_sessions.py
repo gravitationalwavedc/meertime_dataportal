d45abb16ff09cc5ce88bce2238ff2466b8f28bef
NickSwainston <nickaswainston@gmail.com>
2023-06-19 11:47:33 +0800

    Removed the web_cache

--- backend/web_cache/management/commands/sync_sessions.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from web_cache.management.commands._command_functions import sync_sessions
-
-
-class Command(BaseCommand):
-    help = "Sync the web cache session data with the current database"
-
-    def handle(self, *args, **options):
-        sync_sessions()
