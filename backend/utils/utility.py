ee8460ef4ae3e9ef1afc95c5ae2c9083b978e939
Shibli Saleheen <ssaleheen@swin.edu.au>
2023-06-19 10:31:12 +1000

    initial

---
+++ backend/utils/utility.py
@@ -0,0 +1,27 @@
+import os
+import jwt
+import base64
+
+from django.http import HttpResponse
+from django.conf import settings
+
+
+def secure_serve(request, document_root=None, show_indexes=False, *args, **kwargs):
+    """
+    function to return the base64 of the image if a valid jwt token is supplied
+    otherwise - it will return an empty string, meaning no media file.
+    """
+    try:
+        token = request.META['HTTP_AUTHORIZATION'].split(' ')[-1]
+        # expired token for test
+        # token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNzYWxlaGVlbkBzd2luLmVkdS5hdSIsImV4cCI6MTY4NjE5MTg4Niwib3JpZ0lhdCI6MTY4NjE5MTU4Nn0.dRxwWuQ2PAH3fGQhkCKjjtl-3_9c63zkcuCmuZd5oig'
+        jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+
+        file_path = request.path.replace('/media/', '')
+        image_file = os.path.join(document_root, file_path)
+        with open(image_file, "rb") as img_file:
+            b64_string = base64.b64encode(img_file.read())
+        return HttpResponse(b64_string)
+    except Exception as ex:
+        print(ex)
+        return HttpResponse("")
--- frontend/src/components/PlotImage.jsx
+++ frontend/src/components/PlotImage.jsx
@@ -1,23 +1,47 @@
 import Image from "react-bootstrap/Image";
 import image404 from "../assets/images/image404.png";
+import { useEffect, useState } from 'react';
 
 const PlotImage = ({ imageData, handleClick }) => {
-  if (!imageData) return null;
 
-  return (
-    <Image
-      rounded
-      fluid
-      className="mb-3"
-      alt={`Plot ${imageData.plotType} using ${imageData.process} data.`}
-      src={`${import.meta.env.VITE_DJANGO_MEDIA_URL}${imageData.url}`}
-      onError={({ currentTarget }) => {
-        currentTarget.onError = null;
-        currentTarget.src = image404;
-      }}
-      onClick={handleClick}
-    />
-  );
+    const token = localStorage.jwt;
+    const [image, setImage] = useState('');
+    const imageUrl = import.meta.env.VITE_DJANGO_MEDIA_URL + imageData.url;
+
+    useEffect(() => {
+        if (!imageData) {
+            return null;
+        } else {
+            fetch(imageUrl, {
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                },
+            }).then(function (response) {
+                return response.text();
+            }).then((response) => {
+                setImage(response);
+            }).catch((error) => {
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+        }
+    });
+
+    return (
+        <Image
+            rounded
+            fluid
+            className="mb-3"
+            alt={`Plot ${imageData.plotType} using ${imageData.process} data.`}
+            // src={`${import.meta.env.VITE_DJANGO_MEDIA_URL}${imageData.url}`}
+            src={`data:image;base64,${image}`}
+            onError={({ currentTarget }) => {
+                currentTarget.onError = null;
+                currentTarget.src = image404;
+            }}
+            onClick={handleClick}
+        />
+    );
 };
 
 export default PlotImage;
