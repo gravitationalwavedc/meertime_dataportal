c78c0a4c6d14096ffeb7f1542dc9c89944b2afb6
Shibli Saleheen <ssaleheen@swin.edu.au>
2023-07-07 10:21:24 +1000

    refactor code

--- frontend/src/components/PlotImage.jsx
+++ frontend/src/components/PlotImage.jsx
@@ -1,12 +1,11 @@
 import Image from "react-bootstrap/Image";
 import image404 from "../assets/images/image404.png";
 import { useEffect, useState } from 'react';
+import { getImageData } from "../pages/RefreshToken.jsx";
 
 const PlotImage = ({ imageData, handleClick }) => {
 
-    const token = localStorage.jwt;
     const [image, setImage] = useState('');
-    const imageUrl = import.meta.env.VITE_DJANGO_MEDIA_URL + imageData.url;
 
     useEffect(() => {
         if (!imageData) {
@@ -23,18 +22,7 @@ const PlotImage = ({ imageData, handleClick }) => {
                 onClick={handleClick}
             />
         } else {
-            fetch(imageUrl, {
-                headers: {
-                    'Authorization': 'Bearer ' + token,
-                },
-            }).then(function (response) {
-                return response.text();
-            }).then((response) => {
-                setImage(response);
-            }).catch((error) => {
-                // eslint-disable-next-line no-console
-                console.log(error);
-            });
+            getImageData(imageData.url).then(data => setImage(data));
         }
     });
 
@@ -45,7 +33,7 @@ const PlotImage = ({ imageData, handleClick }) => {
             className="mb-3"
             alt={`Plot ${imageData.plotType} using ${imageData.process} data.`}
             // src={`${import.meta.env.VITE_DJANGO_MEDIA_URL}${imageData.url}`}
-            src={`data:image;base64,${image}`}
+            src={image}
             onError={({ currentTarget }) => {
                 currentTarget.onError = null;
                 currentTarget.src = image404;
