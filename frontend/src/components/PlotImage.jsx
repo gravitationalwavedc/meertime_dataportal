f79478fb57d52d331f2025cc7db986ed59dc312f
Asher <jaleslie@swin.edu.au>
2023-08-01 11:15:05 +1000

    Run prettier over project.

--- frontend/src/components/PlotImage.jsx
+++ frontend/src/components/PlotImage.jsx
@@ -1,49 +1,48 @@
 import Image from "react-bootstrap/Image";
 import image404 from "../assets/images/image404.png";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { getImageData } from "../pages/RefreshToken.jsx";
 
 const PlotImage = ({ imageData, handleClick }) => {
+  const [image, setImage] = useState("");
 
-    const [image, setImage] = useState('');
+  useEffect(() => {
+    if (imageData) {
+      getImageData(imageData.url).then((data) => setImage(data));
+    }
+  });
 
-    useEffect(() => {
-        if (imageData) {
-            getImageData(imageData.url).then(data => setImage(data));
-        }
-    });
-
-    return (
-        <>
-        {
-            imageData ? (
-                <Image
-                    rounded
-                    fluid
-                    className="mb-3"
-                    alt={`Plot ${imageData.plotType} using ${imageData.process} data.`}
-                    src={image}
-                    onError={({ currentTarget }) => {
-                        currentTarget.onError = null;
-                        currentTarget.src = image404;
-                    }}
-                    onClick={handleClick}
-                />):(
-                <Image
-                    rounded
-                    fluid
-                    className="mb-3"
-                    alt={`Image not found.`}
-                    src={image404}
-                    onError={({ currentTarget }) => {
-                        currentTarget.onError = null;
-                        currentTarget.src = image404;
-                    }}
-                    onClick={handleClick}
-                />)
-        }
-        </>
-    );
+  return (
+    <>
+      {imageData ? (
+        <Image
+          rounded
+          fluid
+          className="mb-3"
+          alt={`Plot ${imageData.plotType} using ${imageData.process} data.`}
+          src={image}
+          onError={({ currentTarget }) => {
+            currentTarget.onError = null;
+            currentTarget.src = image404;
+          }}
+          onClick={handleClick}
+        />
+      ) : (
+        <Image
+          rounded
+          fluid
+          className="mb-3"
+          alt={`Image not found.`}
+          src={image404}
+          onError={({ currentTarget }) => {
+            currentTarget.onError = null;
+            currentTarget.src = image404;
+          }}
+          onClick={handleClick}
+        />
+      )}
+    </>
+  );
 };
 
 export default PlotImage;
--- frontend/src/pages/RefreshToken.jsx
+++ frontend/src/pages/RefreshToken.jsx
@@ -13,40 +13,43 @@ const refreshTokenMutation = graphql`
 `;
 
 export function performRefreshTokenMutation(router) {
-    const variables = {
-        token: localStorage.jwt,
-    };
+  const variables = {
+    token: localStorage.jwt,
+  };
 
-    commitMutation(environment, {
-        mutation: refreshTokenMutation,
-        variables,
-        onCompleted: ({ refreshToken }, errors) => {
-            if (errors) {
-                localStorage.jwt = '';
-                router.replace("/login/");
-            } else if (refreshToken) {
-                localStorage.jwt = refreshToken.token;
-            }
-        },
-        onError: (error) => {
-            localStorage.jwt = '';
-            router.replace("/login/");
-        },
-    });
+  commitMutation(environment, {
+    mutation: refreshTokenMutation,
+    variables,
+    onCompleted: ({ refreshToken }, errors) => {
+      if (errors) {
+        localStorage.jwt = "";
+        router.replace("/login/");
+      } else if (refreshToken) {
+        localStorage.jwt = refreshToken.token;
+      }
+    },
+    onError: (error) => {
+      localStorage.jwt = "";
+      router.replace("/login/");
+    },
+  });
 }
 
 export function getImageData(imageUrl) {
-    const mediaUrl = import.meta.env.VITE_DJANGO_MEDIA_URL + imageUrl
-    return fetch(mediaUrl, {
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.jwt,
-        },
-    }).then(function (response) {
-        return response.text();
-    }).then((response) => {
-        return `data:image;base64,${response}`;
-    }).catch((error) => {
-        // eslint-disable-next-line no-console
-        console.log(error);
+  const mediaUrl = import.meta.env.VITE_DJANGO_MEDIA_URL + imageUrl;
+  return fetch(mediaUrl, {
+    headers: {
+      Authorization: "Bearer " + localStorage.jwt,
+    },
+  })
+    .then(function (response) {
+      return response.text();
+    })
+    .then((response) => {
+      return `data:image;base64,${response}`;
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.log(error);
     });
 }
