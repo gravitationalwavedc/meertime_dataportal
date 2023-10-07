67f5f273e51c6d3d9b8f863a31a267395b7605c0
Nick Swainston <nickaswainston@gmail.com>
2023-10-07 07:36:26 +0000

    Found a toa bug where it didn't correctly remove trailing zeros

--- backend/utils/tests/test_toa.py
+++ backend/utils/tests/test_toa.py
@@ -12,6 +12,9 @@ def test_toa_line_to_dict_to_line():
         "J1705-1903_2020-12-24-07:06:49_zap.4ch1p12t.ar.tim",
         "J1337-6423_2019-07-01-15:12:35_zap.16ch1p1t.ar.dm_corrected.tim",
         "J1644-4559_2019-09-06-14:27:14_zap.1ch1p1t.ar.tim",
+        "J0835-4510_2019-04-17-22:39:05_zap.1ch1p1t.ar.tim",
+        "J0437-4715_2019-03-26-16:29:52_zap.1ch1p1t.ar.tim",
+        "J0437-4715_2022-12-02-03:18:27_zap.1ch1p1t.ar.tim"
     ]
     for test in tests:
         toa_file = os.path.join(TEST_DATA_DIR, test)
@@ -21,7 +24,4 @@ def test_toa_line_to_dict_to_line():
                 input_toa_line = toa_line.rstrip("\n")
                 toa_dict = toa_line_to_dict(input_toa_line)
                 output_toa_line = toa_dict_to_line(toa_dict)
-                print(input_toa_line)
-                print(output_toa_line)
-                print(toa_dict)
                 assert input_toa_line == output_toa_line
