890a43825591d2366b9dcb385938e5ecb2c4ee99
Nick Swainston <nickaswainston@gmail.com>
2023-11-21 07:20:52 +0000

    Fixed cypress tests

--- frontend/cypress/e2e/singleObservation.cy.js
+++ frontend/cypress/e2e/singleObservation.cy.js
@@ -45,26 +45,6 @@ describe("Single Observation Page", () => {
     cy.contains("Download Data Files").should("be.visible");
   });
 
-  it("should hide download buttons when there are no files", () => {
-    cy.intercept("http://localhost:8000/graphql/", (req) => {
-      aliasQuery(
-        req,
-        "SingleObservationQuery",
-        "foldObservationDetailsNoFiles.json"
-      );
-    });
-
-    cy.visit("/J0125-2327/2020-02-04-00:21:21/2/");
-
-    cy.wait("@SingleObservationQuery");
-
-    // Correct page loads
-    cy.contains("J0125-2327").should("be.visible");
-
-    // Download buttons should not show.
-    cy.contains("Download Data Files").should("not.exist");
-  });
-
   it("should render the page with images", () => {
     cy.wait("@SingleObservationQuery");
 
