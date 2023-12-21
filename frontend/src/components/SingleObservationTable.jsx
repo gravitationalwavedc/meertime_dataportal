29a2ed00dc060051c76018ac1f1b12d69f62efb0
Nick Swainston <nickaswainston@gmail.com>
2023-12-21 02:51:47 +0000

    Now show SNR, DM and RM on single obs page

--- frontend/src/components/SingleObservationTable.jsx
+++ frontend/src/components/SingleObservationTable.jsx
@@ -44,6 +44,11 @@ const SingleObservationTableFragment = graphql`
             foldTsubint
             nant
           }
+          pipelineRun {
+            dm
+            rm
+            sn
+          }
           images {
             edges {
               node {
@@ -95,6 +100,9 @@ const SingleObservationTable = ({ data, jname }) => {
     Nbin: relayObservationModel.observation.foldNbin,
     Nchan: relayObservationModel.observation.foldNchan,
     "Subint Time (s)": relayObservationModel.observation.foldTsubint,
+    "SNR": relayObservationModel.pipelineRun.sn !== null ? relayObservationModel.pipelineRun.sn.toFixed(1) : null,
+    "DM (pc cm^-3)": relayObservationModel.pipelineRun.dm !== null ? relayObservationModel.pipelineRun.dm.toFixed(4) : null,
+    "RM (rad m^-2)": relayObservationModel.pipelineRun.rm !== null ? relayObservationModel.pipelineRun.rm.toFixed(4) : null,
   };
 
   const isMolonglo = relayObservationModel.observation.project.mainProject.name
