b1f0c1ec11eeb2a4d62e1758d746aae737895d1d
Nick Swainston <nickaswainston@gmail.com>
2024-02-12 03:01:38 +0000

    Added a debugtoolbar url so it works for development

--- backend/meertime/settings/django_debug_toolbar.py
+++ backend/meertime/settings/django_debug_toolbar.py
@@ -7,5 +7,5 @@ INSTALLED_APPS.append('graphiql_debug_toolbar')
 # MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
 MIDDLEWARE.append("graphiql_debug_toolbar.middleware.DebugToolbarMiddleware")
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [env("INTERNAL_IPS", default="127.0.0.1"), "10.0.2.2", "0.0.0.0"]
+INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [env("INTERNAL_IPS", default="127.0.0.1"), "10.0.2.2", "0.0.0.0", "localhost"]
 SHOW_TOOLBAR_CALLBACK=True
