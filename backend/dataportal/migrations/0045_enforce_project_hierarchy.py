f7b929bb812bcf21889e8c96e4d851bf84e4403b
Asher <jasherleslie@gmail.com>
2026-07-08 13:09:07 +1000

    Allow telescopes to have multiple MainProjects

--- backend/dataportal/migrations/0045_enforce_project_hierarchy.py
+++ backend/dataportal/migrations/0045_enforce_project_hierarchy.py
@@ -11,9 +11,9 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="mainproject",
             name="telescope",
-            field=models.OneToOneField(
+            field=models.ForeignKey(
                 on_delete=django.db.models.deletion.CASCADE,
-                related_name="main_project",
+                related_name="main_projects",
                 to="dataportal.telescope",
             ),
         ),
--- backend/dataportal/tests/test_project_hierarchy.py
+++ backend/dataportal/tests/test_project_hierarchy.py
@@ -5,12 +5,12 @@ from dataportal.models import MainProject, Project, Telescope
 
 
 class ProjectHierarchyTest(TestCase):
-    def test_telescope_has_only_one_main_project(self):
+    def test_telescope_can_have_multiple_main_projects(self):
         telescope = Telescope.objects.create(name="Synthetic Scope")
-        MainProject.objects.create(name="First", telescope=telescope)
+        first = MainProject.objects.create(name="First", telescope=telescope)
+        second = MainProject.objects.create(name="Second", telescope=telescope)
 
-        with self.assertRaises(IntegrityError), transaction.atomic():
-            MainProject.objects.create(name="Second", telescope=telescope)
+        self.assertEqual(list(telescope.main_projects.order_by("id")), [first, second])
 
     def test_project_requires_main_project(self):
         with self.assertRaises(IntegrityError), transaction.atomic():
